cbfe23a8d3f48ad76fe752bf12eb4cf3aa0c5f9c
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-07-28 10:40:20 +0530

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,1 +1,19 @@
-. 
+const matches = [
+  {
+    date: "18/06/2025",
+    opponent: "DAU XI",
+    venue: "Ramkali Stadium, Shuklaganj",
+    overs: "20",
+    toss: "",
+    teamScore: "N/A",
+    opponentScore: "N/A",
+    result: "Abandoned due to Heat",
+    playerOfTheMatch: "",
+    bestBatter: "",
+    bestBowler: "",
+    highlights: [
+      "Match abandoned due to extreme heat."
+    ],
+    photos: []
+  }
+];
