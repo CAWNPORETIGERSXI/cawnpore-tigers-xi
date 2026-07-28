1ba911e3c3bc05a539300560bbd49912468a61b9
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-07-28 20:56:38 +0530

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,73 +1,86 @@
 const matches = [
-
-  {
-    id: 1,
-    date: "18/06/2025",
-    opponent: "DAU XI",
-    venue: "Ramkali Stadium, Shuklaganj",
-    overs: "20",
-    toss: "",
-    teamScore: "N/A",
-    opponentScore: "N/A",
-    result: "Abandoned due to Heat",
-    playerOfTheMatch: "",
-    bestBatter: "",
-    bestBowler: "",
-    highlights: [
-      "Match abandoned due to extreme heat."
-    ],
-    photos: []
-  },
-
-  {
-    id: 2,
-    date: "29/06/2025",
-    opponent: "DAU XI",
-    venue: "Ramkali Stadium, Shuklaganj",
-    overs: "18",
-    toss: "",
-    teamScore: "180/5 (18 Overs)",
-    opponentScore: "130/10 (17.3 Overs)",
-    result: "Won by 50 Runs",
-    playerOfTheMatch: "Naveen",
-    bestBatter: "Naveen",
-    bestBowler: "Vivek Dwivedi",
-    highlights: [],
-    photos: []
-  },
-
-  {
-    id: 3,
-    date: "12/10/2025",
-    opponent: "WE XI",
-    venue: "Diversity Sport Arena",
-    overs: "22",
-    toss: "",
-    teamScore: "112/10 (19.3 Overs)",
-    opponentScore: "179/3 (22 Overs)",
-    result: "Lost by 67 Runs",
-    playerOfTheMatch: "Saurabh",
-    bestBatter: "Swastick",
-    bestBowler: "Saurabh",
-    highlights: [],
-    photos: []
-  },
-
-  {
-    id: 4,
-    date: "20/10/2025",
-    opponent: "Team Diamond",
-    venue: "Rashtriya Ground",
-    overs: "23",
-    toss: "",
-    teamScore: "151/6 (21.2 Overs)",
-    opponentScore: "150/10 (23 Overs)",
-    result: "Won by 4 Wickets",
-    playerOfTheMatch: "Naveen",
-    bestBatter: "Amit",
-    bestBowler: "Naveen",
-    highlights: [],
-    photos: []
-  }
-
+{
+ date: "19/07/2026",
+ opponent: "Madhyamik Lions",
+ overs: "25",
+ result: "Won by 2 wickets"
+},
+{
+ date: "26/06/2026",
+ opponent: "MSS Youth Star",
+ overs: "25",
+ result: "Lost by 7 wickets"
+},
+{
+ date: "20/06/2026",
+ opponent: "Strike Squad XI",
+ overs: "25",
+ result: "Lost by 4 wickets"
+},
+{
+ date: "18/06/2026",
+ opponent: "MSS Youth Star XI",
+ overs: "25",
+ result: "Won by 6 wickets"
+},
+{
+ date: "13/06/2026",
+ opponent: "Strike Squad XI",
+ overs: "25",
+ result: "Abandoned due to rain"
+},
+{
+ date: "10/06/2026",
+ opponent: "County Stars",
+ overs: "20",
+ result: "Won by 7 wickets"
+},
+{
+ date: "06/06/2026",
+ opponent: "Strike Squad",
+ overs: "20",
+ result: "Lost by 9 runs"
+},
+{
+ date: "03/06/2026",
+ opponent: "Madhyamik Lions",
+ overs: "20",
+ result: "Won by 1 wicket"
+},
+{
+ date: "17/05/2026",
+ opponent: "Kanpur Cricket Warriors",
+ overs: "25",
+ result: "Won by 6 wickets"
+},
+{
+ date: "02/03/2026",
+ opponent: "Unnao Panthers",
+ overs: "25",
+ result: "Won by 6 wickets"
+},
+{
+ date: "20/10/2025",
+ opponent: "Team Diamond",
+ overs: "25",
+ result: "Won by 4 wickets"
+},
+{
+ date: "12/10/2025",
+ opponent: "WE XI",
+ overs: "22",
+ result: "Lost by 67 runs"
+},
+{
+ date: "29/06/2025",
+ opponent: "DAU XI",
+ overs: "18",
+ result: "Won by 50 runs"
+},
+{
+ date: "18/06/2025",
+ opponent: "DAU XI",
+ overs: "20",
+ result: "Abandoned due to heat"
+}
 ];
