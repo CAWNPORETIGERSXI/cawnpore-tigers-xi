44cb899714f983b2e31ab12ab621fea3227e6f9f
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-07-28 18:06:38 +0530

    Update matches.js

--- matches.js
+++ matches.js
@@ -33,3 +33,40 @@ const matches = [
     photos: []
   }
 ];
+const matches = [
+  // Match 1
+
+  // Match 2
+
+  {
+    id: 3,
+    date: "12/10/2025",
+    opponent: "WE XI",
+    venue: "Diversity Sport Arena",
+    overs: "22",
+    toss: "",
+    teamScore: "112/10 (19.3 Overs)",
+    opponentScore: "179/3 (22 Overs)",
+    result: "Lost by 67 Runs",
+    playerOfMatch: "Saurabh",
+    bestBatter: "Swastick",
+    bestBowler: "Saurabh"
+  },
+
+  {
+    id: 4,
+    date: "20/10/2025",
+    opponent: "Team Diamond",
+    venue: "Rashtriya Ground",
+    overs: "23",
+    toss: "",
+    teamScore: "151/6 (21.2 Overs)",
+    opponentScore: "150/10 (23 Overs)",
+    result: "Won by 4 Wickets",
+    playerOfMatch: "Naveen",
+    bestBatter: "Amit",
+    bestBowler: "Naveen"
+  }
+
+  // Match 5...
+];
