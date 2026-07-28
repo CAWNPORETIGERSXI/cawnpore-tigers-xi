3ccca6006c3e2988af3192c44b4e5a646ae1f250
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-07-28 14:39:14 +0530

    Update matches.js

--- matches.js
+++ matches.js
@@ -15,20 +15,21 @@ const matches = [
       "Match abandoned due to extreme heat."
     ],
     photos: []
+  },
+
+  {
+    date: "29/06/2025",
+    opponent: "DAU XI",
+    venue: "Ramkali Stadium, Shuklaganj",
+    overs: "18",
+    toss: "",
+    teamScore: "180/5 (18 Overs)",
+    opponentScore: "130/10 (17.3 Overs)",
+    result: "Won by 50 Runs",
+    playerOfTheMatch: "Naveen",
+    bestBatter: "Naveen",
+    bestBowler: "Vivek Dwivedi",
+    highlights: [],
+    photos: []
   }
 ];
-{
-  date: "29/06/2025",
-  opponent: "DAU XI",
-  venue: "Ramkali Stadium, Shuklaganj",
-  overs: "18",
-  toss: "",
-  teamScore: "180/5 (18 Overs)",
-  opponentScore: "130/10 (17.3 Overs)",
-  result: "Won by 50 Runs",
-  playerOfTheMatch: "Naveen",
-  bestBatter: "Naveen",
-  bestBowler: "Vivek Dwivedi",
-  highlights: [],
-  photos: []
-}
