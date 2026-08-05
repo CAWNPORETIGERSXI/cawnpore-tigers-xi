29368745946445230e30e9047b6509d530507a12
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-08-05 11:07:16 +0530

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,173 +1,34 @@
-const matches = [
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-{
- date: "18/06/2025",
- opponent: "DAU XI",
- overs: "20",
- result: "Abandoned due to heat",
- venue: "RAMKALI STADIUM"
-},
+const table = document.getElementById("matchTable");
 
-{
- date: "29/06/2025",
- opponent: "DAU XI",
- overs: "18",
- result: "CAWNPORE TIGERS XI WON BY 50 RUNS",
- venue: "RAMKALI STADIUM"
-},
+async function loadMatches() {
+  table.innerHTML = "";
 
-{
- date: "12/10/2025",
- opponent: "WE XI",
- overs: "22",
- result: "WE XI WON BY 67 RUNS",
- venue: "DIVERAITY STADIUM"
-},
+  const q = query(collection(db, "matches"), orderBy("createdAt", "asc"));
+  const snapshot = await getDocs(q);
 
-{
- date: "20/10/2025",
- opponent: "TEAM DIAMOND XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI WON BY 4 WICKETS",
- venue: "RASTRIYA STADIUM",
- playerOfMatch: "NAVEEN",
- bestBatter: "AMIT",
- bestBowler: "ANNA",
- summaryImage: "match11-summary.jpg"
-},
+  let sr = 1;
 
-{
- date: "02/03/2026",
- opponent: "UNNAO PANTHERS",
- overs: "25",
- result: "CAWNPORE TIGERS XI WON BY 6 WICKETS",
- venue: "RAMKALI STADIUM"
-},
+  snapshot.forEach((doc) => {
+    const m = doc.data();
 
-{
- date: "17/05/2026",
- opponent: "KANPUR WARRIOR XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI WON BY 6 WICKETS",
- venue: "NARAYAN STADIUM"
-},
-
-{
- date: "03/06/2026",
- opponent: "MADHYAMIK LIONS",
- overs: "20",
- result: "CAWNPORE TIGERS XI WON BY 1 WICKET",
- venue: "GEMS STADIUM"
-},
-
-{
- date: "06/06/2026",
- opponent: "STRIKE SQUAD XI",
- overs: "20",
- result: "CAWNPORE TIGERS XI LOST BY 9 RUNS",
- venue: "EVEREST CRICKET STADIUM"
-},
-
-{
- date: "10/06/2026",
- opponent: "COUNTY STARS XI",
- overs: "20",
- result: "CAWNPORE TIGERS XI WON BY 7 WICKETS",
- venue: "SCG STADIUM",
- playerOfMatch: "PURAN",
- bestBatter: "JISHAN",
- bestBowler: "PRAVEEN",
- summaryImage: "match6-summary.jpg"
-},
-
-{
- date: "13/06/2026",
- opponent: "STRIKE SQUAD XI",
- overs: "25",
- result: "MATCH ABANDONED DUE TO RAIN",
- venue: "SCG STADIUM"
-},
-
-{
- date: "18/06/2026",
- opponent: "MSS YOUTH XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI WON BY 6 WICKETS",
- venue: "EVEREST STADIUM"
-},
-
-{
- date: "20/06/2026",
- opponent: "STRIKE SQUAD XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI LOST BY 4 WICKETS",
- venue: "EVEREST STADIUM"
-},
-
-{
- date: "26/06/2026",
- opponent: "MSS YOUTH XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI LOST BY 7 WICKETS",
- venue: "EVEREST STADIUM"
-},
-
-{
- date: "19/07/2026",
- opponent: "MADHYAMIK LIONS XI",
- overs: "25",
- result: "CAWNPORE TIGERS XI WON BY 2 WICKETS",
- venue: "RAMKALI STADIUM",
- playerOfMatch: "JISHAN",
- bestBatter: "JISHAN",
- bestBowler: "DEEPAK",
- summaryImage: "match14-summary.jpg"
-},
-
-
-{
-date: "02/08/2026",
-opponent: "COUNTY STARS",
-overs: "25",
-result: "CAWNPORE TIGERS XI WON BY 2 WICKETS",
-venue: "RAMKALI STADIUM",
-playerOfMatch: "ANNA",
-bestBatter: "JISHAN",
-bestBowler: "VINAY",
-summaryImage: "match15-summary.jpg"
-},
-{
- date: "09/08/2026",
- opponent: "THE ROYAL STARS",
- overs: "25",
- result: "UPCOMING",
- venue: "RAMKALI STADIUM"
-},
-
-{
- date: "16/08/2026",
- opponent: "MSS YOUTH STARS",
- overs: "25",
- result: "UPCOMING",
- venue: "RAMKALI STADIUM"
-},
-
-{
- date: "26/08/2026",
- opponent: "GAJRAJ LEGION",
- overs: "25",
- result: "UPCOMING",
- venue: "RAMKALI STADIUM"
-},
-
-{
- date: "30/08/2026",
- opponent: "WISHDOM WARRIOR",
- overs: "25",
- result: "UPCOMING",
- venue: "RAMKALI STADIUM"
+    table.innerHTML += `
+      <tr onclick="window.open('https://cricheroes.com/', '_blank')">
+        <td>${sr++}</td>
+        <td>${m.date || ""}</td>
+        <td>${m.opponent || ""}</td>
+        <td>${m.overs || ""}</td>
+        <td>${m.result || ""}</td>
+      </tr>
+    `;
+  });
 }
 
-];
-
-export { matches };
+loadMatches();
