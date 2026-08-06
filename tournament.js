5492732761d374c8df319a6f5be53f24921d2a8a
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-08-06 12:12:01 +0530

    Create tournament.js

---
+++ tournament.js
@@ -0,0 +1,43 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs,
+  query,
+  where
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const table = document.getElementById("matchesTable");
+
+async function loadTournamentMatches() {
+
+  table.innerHTML = "";
+
+  const q = query(
+    collection(db, "matches"),
+    where("eventId", "==", "TOURNAMENT")
+  );
+
+  const snapshot = await getDocs(q);
+
+  let no = 1;
+
+  snapshot.forEach((doc) => {
+
+    const match = doc.data();
+
+    table.innerHTML += `
+      <tr onclick="location.href='match-summary.html?id=${doc.id}'">
+        <td>${no++}</td>
+        <td>${match.date || ""}</td>
+        <td>${match.opponent || ""}</td>
+        <td>${match.venue || ""}</td>
+        <td>${match.result || ""}</td>
+      </tr>
+    `;
+
+  });
+
+}
+
+loadTournamentMatches();
