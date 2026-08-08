728a2036887d1e48f7f25753bd8411afeae51bb1
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-08-08 22:21:21 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,6 +10,11 @@ import {
     getDocs
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 // ======================================================
 // LOGIN
